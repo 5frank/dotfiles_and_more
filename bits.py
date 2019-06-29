cfe9da252ce8f2af16efe1a7a31d2d6708639367
sfrank <sfrank@tanis>
2019-06-29 11:59:40 +0200

    pretty bits

--- bits.py
+++ bits.py
@@ -1,115 +1,130 @@
-#!/usr/bin/env python3 
 import argparse
-import sys
-from math import ceil, log
+from sys import stderr
+from struct import pack as struct_pack
 
-def _toint(x):
-    return int(x, 0)
+# IEEE754 floating point/ '<'=little, '>'=big
+STRUCT_FMT_FP = {
+    16: '>e',
+    32: '>f',
+    64: '>d'
+}
 
-def eprint(*args, **kwargs):
-    ''' error print '''
-    print(*args, file=sys.stderr, **kwargs)
-
-def vprint(*args, **kwargs):
-    ''' verbose print '''
-    if g_args.verbose >= 1:
-        print(*args, file=sys.stdout, **kwargs)
-
-parser = argparse.ArgumentParser(
-        description='Pretty print bits. Like hexdump but for bits')
-
-parser.add_argument('values', metavar='INTVAL', type=str, nargs='+',
-    help='''One or more hex- or decimal integer values. Can be mixed format.''')
-
-parser.add_argument('--wordsize', '-w', metavar='N', type=int, default=32,
-    choices=[8, 12, 16, 24, 32, 48, 64, 128], 
-    help='''Word size and number of bits per line. Will be overridden if
-    passed value(s) do not fit''')
-#and always rounded up to closest multiple of four, otherwise default to 32''')
-
-parser.add_argument('--bigend', '-b', action='store_true', default=False, 
-    help='big endianess (byteorder). little is default. confusing results')
-
-parser.add_argument('--hex', '-x', action='store_true', default=False, 
-    help='All inputs interpreted as hexadecimal')
-
-parser.add_argument('--revbit', '-r', type=str, default='', 
-     choices=['word', 'byte','w','W', 'b', 'B'], 
-     help='''Reverse bit order byte- or word-wise. Not same as changed endianess''')
-
-
-'''
-
-parser.add_argument('--byteorder', '--endian', '-e', type=str, default='little', 
-        choices=['big', 'little'], 
-        help='endianess (eg. byteorder) of input and output.')
-'''
-
-parser.add_argument('--verbose', '-v', default=0, action='count',
-    help='Verbose output (debug)')
-
-def ceilmul(x, n):
-    ''' round up to closest multiple of n '''
-    r = x % n
-    return x if r == 0 else (x + n - r)
-
-def strsplitnth(s, n):
-    ''' split string every n:th character '''
-    return [s[i:i+n] for i in range(0, len(s), n)]
+def print_err(*args, **kwargs):
+    print('E:', *args, file=stderr, **kwargs)
 
 def revbytebits(b):
+    ''' reverse bits in a single byte '''
     b = ((b & 0xF0) >> 4) | ((b & 0x0F) << 4)
     b = ((b & 0xCC) >> 2) | ((b & 0x33) << 2)
     b = ((b & 0xAA) >> 1) | ((b & 0x55) << 1)
     return b
 
-def convertword(v, nbytes, bigend=False, revbit=''):
-    #order = 'little' if bigend else 'big' 
-    ba = v.to_bytes(nbytes, byteorder=sys.byteorder)
-
-    if revbit.startswith('w'):
-        ba = [revbytebits(b) for b in reversed(ba)]
-
-    elif revbit.startswith('b'):
-        ba = [revbytebits(b) for b in ba]
-
-    order = 'big' if bigend else 'little' 
-    return int.from_bytes(ba, byteorder=order)
-
-def parsevalues(args):
-    vals = [None] * len(args.values)
-    base = 16 if args.hex else 0
-    for i, s in enumerate(args.values):
-        vals[i] = int(s, base)
-    return vals
-    
-def main():
-    args = parser.parse_args()
-    values = parsevalues(args)
-
-    nbitsrequired = max(values).bit_length() 
-    nbits = max(8, args.wordsize, nbitsrequired)
-    nbits = ceilmul(nbits, 4)
-
-    nbytes = max(1, int((nbits + 4) / 8.0))
-    nhexdig = nbytes * 2
-    args.revbit = args.revbit.lower()
-
-    if args.verbose:
-        print(args)
-        print('nbits: ', nbits, ' required: ', nbitsrequired)
-        print('nbytes: ', nbytes)
-
-    # bit postion header 
+def header(nbits):
     rev4 = reversed(range(0, nbits, 4))
     nibidxs = ['{0: 4d} '.format(n) for n in rev4]
-    print(''.join(nibidxs))
+    return ''.join(nibidxs)
 
-    for v in values:
-        v = convertword(v, nbytes, args.bigend, args.revbit)
-        s = '{:0{}b}'.format(v, nbits)
-        nibs = strsplitnth(s, 4)
-        hexdec = '  |0x{:0{}X}  {: >8}|'.format(v, nhexdig, v)
-        print(' '.join(nibs), hexdec)
+class Word(object):
+    ''' "Word" as in series of bytes and bits '''
+
+    def __init__(self, sval, nbits=32, base=0):
+
+        # always use big endian as bits displayed w MSB first, 
+        # i.e. N, N-1, ... 2, 1, 0. no need to reverse later
+        self.nbytes = int(nbits / 8)
+        if '.' in sval:
+            val = float(sval)
+            if nbits not in STRUCT_FMT_FP:
+                print_err('wordsize', nbits, 
+                        'can not be used with floating point')
+                exit(1)
+
+            self.ba = struct_pack(STRUCT_FMT_FP[nbits], val)
+        else:
+            val = int(sval, base)
+            self.ba = val.to_bytes(self.nbytes,
+                    signed = val < 0, 
+                    byteorder='big')
+
+        self.sval = sval
+        self.val = val
+
+    def revbytes(self):
+        self.ba.reverse()
+
+    def revbits(self, bytewise=False):
+        src = reversed(self.ba) if bytewise else self.ba
+        self.ba = bytearray([revbytebits(b) for b in src])
+
+    def invert(self):
+        self.ba = bytearray([(~b & 0xFF) for b in self.ba])
+
+    def nibstr(self):
+        bNibs = lambda x : '{:04b} {:04b}'.format((x >> 4) & 0xf, x & 0xf)
+        a = [bNibs(b) for b in self.ba]
+        return ' '.join(a)
+
+    def hexstr(self):
+        a = ['{:02X}'.format(b) for b in self.ba]
+        return '0x' + ''.join(a)
+
+    def __str__(self):
+        return '{} | {} | {}'.format(self.nibstr(), self.hexstr(), self.sval)
+
+parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description='print pretty bits with extras.')
+
+parser.add_argument('values', metavar='VAL', nargs='+',
+        help='One or more values in hex, decimal or floating point \
+        format.  Formats can be mixed.  \
+        floating point interpreted as IEEE754 16, 32 or 64 bit floats \
+        depending on wordsize')
+
+parser.add_argument('--wordsize', '-w', metavar='N', type=int, default=32,
+        choices=[8, 16, 24, 32, 64, 128], 
+        help='Word size in bits and also number of bits per line. \
+                must be a multiple of 8')
+
+parser.add_argument('--hex', '-x', action='store_true', 
+        help='All inputs interpreted as hexadecimal')
+
+parser.add_argument('--invert', '-i', action='store_true', 
+        help='invert bits')
+
+parser.add_argument('--reverse', '-r', default=None, 
+        choices=['word', 'byte','w','W', 'b', 'B'], 
+        help='Reverse bit order byte- or word-wise.')
+
+
+def main():
+    args = parser.parse_args()
+
+    args.wordsize = int(args.wordsize)
+    if args.reverse:
+        c = args.reverse[0].lower()
+        revbitsbytewise = True if c == 'w' else False
+    else:
+        revbitsbytewise = None
+
+    base = 16 if args.hex else 0
+
+    words = [None] * len(args.values)
+    for i, s in enumerate(args.values):
+        w = Word(s, nbits=args.wordsize, base=base)
+
+        if revbitsbytewise is not None:
+            w.revbits(bytewise=revbitsbytewise)
+
+        if args.invert:
+            w.invert()
+
+        words[i] = w
+
+    print(header(args.wordsize))
+    for w in words:
+        print(w)
+    exit(0)
+
 
 main()
--- lsserial.py
+++ lsserial.py
@@ -4,6 +4,7 @@ from serial.tools.list_ports import comports
 from glob import glob
 from os import path
 
+from collections import OrderedDict
 import argparse
 '''
 major numbers:
@@ -19,11 +20,10 @@ useful commands:
     ls /sys/bus/serial/devices/
 
 '''
-
 def busid_from_device(device):
     ''' get USB bus id from a device (ex. /dev/ttyUSB) '''
 
-    device = path.realpath(device)# expand symlink
+    device = path.realpath(device)# expand symlink. needed?
     basename = path.basename(device)
 
     matches = glob('/sys/bus/usb/devices/*/{}'.format(basename))
@@ -36,43 +36,135 @@ def busid_from_device(device):
     busid = path.basename(parent)
     return busid
 
-def usb_tree(devices):
-    
-    path = '/sys/bus/usb/devices/*/{}'.format(basename)
-    matches = glob(path)
-    parent = path.abspath(path.join(path, '..'))
-    busid = path.basename(parent)
+def busid_to_vidpid(busid):
+    ''' return true if usb bus ID matches VID and PID '''
+    if busid is None:
+        return (None, None)
 
+    if busid.startswith('usb'): # USB controllers or root hub
+        return (None, None)
+
+    if ':' in busid: # strip ":<config>.<interface>"
+        busid = busid.split(':')[0] # from "3-1:1.2" to "3-1"
+
+    def readid(_basepath, _idfile):
+        p = path.join(_basepath, _idfile)
+
+        if not path.exists(p):
+            return None
+        with open(p) as f:
+            idX = f.readline()
+        return idX.strip()
+
+    basepath = path.join('/sys/bus/usb/devices/', busid)
+
+    vid = readid(basepath, 'idVendor')
+    pid = readid(basepath, 'idProduct')
+    return (vid, pid)
+
+class SerialDeviceInfo(object):
+    def __init__(self, devname, descr):
+        self.devname = devname
+        self.descr = descr
+        self.busid = busid_from_device(devname)
+        vid, pid = busid_to_vidpid(self.busid)
+        self.usbvid = vid
+        self.usbpid = pid
+
+def deviceInfo(devname, descr):
+    d = OrderedDict()
+    d['devname']= devname
+    d['descr']= descr
+    busid = busid_from_device(devname)
+    vid, pid = busid_to_vidpid(busid)
+    d['busid']= busid
+    d['usb_vid']= vid
+    d['usb_pid']= pid
+
+def columnwidth(rows, i):
+    return max([len(row[i]) for row in rows])
+
+def printaligned(rows):
+
+    ncols = len(rows[0])
+    if ncols < 1:
+        return
+
+    if ncols == 1:
+        for row in rows:
+            print(row[0]) 
+        return
+
+    colfmt = ''
+    for i in range(0, ncols - 1):
+        width = columnwidth(rows, i)
+        colfmt += '{: <' + str(width) + '}  ' 
+    colfmt += '{}'
+
+    for row in rows:
+         #print('{: <16} {: <16} {}'.format(*row))
+         line = colfmt.format(*row)
+         print(line)
 
 
 def main():
-    '''
     parser = argparse.ArgumentParser(description='List serial ports',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--usb', '-u', action='store_true', default=False, 
+	help='show and sort by usb bus id. (GNU/Linux only)')
+
+    '''
 
     parser.add_argument('--all', '-a', action='store_true', default=False, 
         help='include extra info, serial ports not binded to a device etc')
 
-    parser.add_argument('--tree', '-t', action='store_true', default=False, 
-	help='try list as tree. mostly relevant for usb-serial devices')
+
 
     parser.add_argument('--verbose', '-v', default=0, action='count',
         help='Verbose output')
     '''
+    parser.add_argument('--test', action='store_true', default=False)
+
+    parser.add_argument('--usb-pid', type=str, action='append', default=[], 
+            help='Match USB product ID(s). Hex format')
+
+    parser.add_argument('--usb-vid', type=str, action='append', default=[], 
+            help='Match USB vendor ID(s). Hex format')
 
     args = parser.parse_args()
-    cols = [] 
-    for port in comports(): 
-        device = port[0]
-        descr = port[1]
-        busid = busid_from_device(device)
-        cols.append([busid, device, descr])
-        cols.append([busid, device, descr])
-	
-       
-    for row in sorted(cols):
-         print('{: <16} {: <16} {}'.format(*row))
-       
+    if args.test:
+        ports = [
+            ['/dev/ttyUSB0', 'Quad RS232-HS'], 
+            ['/dev/ttyUSB1', 'Quad RS232-HS'], 
+            ['/dev/ttyUSB2', 'Quad RS232-HS'], 
+            ['/dev/ttyUSB3', 'Quad RS232-HS']]
+    else: 
+        ports = comports()
+
+    #rows = [] 
+    sdis = []
+    for port in ports:
+        sdi = deviceInfo(devname=port[0], descr=port[1]) 
+        sdis.append(sdi)
+
+        # device = port[0]
+        # descr = port[1]
+        # cells = []
+        # if args.usb:
+            # busid = busid_from_device(device) 
+            # cells.append(busid)
+        # cells.extend([device, descr])
+        # rows.append(cells)
+
+    if len(sdis) == 0:
+        return 0
+
+
+    #sdis = sorted(sdis)
+    print(sdis)
+    #printaligned(sdis)
+
 
 if __name__ == '__main__':
     main()
