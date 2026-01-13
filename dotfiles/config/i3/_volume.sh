b78a3614ac4eddb6579590db7b1f383d46cceabc
Simon Frank <simon.frank@lohmega.com>
2026-01-13 11:57:55 +0100

    use pipwire wpctl instead of alsamixer

--- dotfiles/config/i3/_volume.sh
+++ dotfiles/config/i3/_volume.sh
@@ -2,14 +2,14 @@
 
 # Volume control etc. refresh  killall -USR1 i3status
 
-updatei3bar()
+_updatei3bar()
 {
   # assumes same signal used in i3blocks config
   pkill -RTMIN+10 i3blocks
   #killall -USR1 i3status
 }
 
-printvolume_unicode()
+_printvolume_unicode()
 {
   # get evertyning within brackets
   local chstatus="$(amixer get Master | grep -oE '\[(.*?)\]' | tr -d []%)"
@@ -27,7 +27,7 @@ printvolume_unicode()
   env printf "$volumeicon%3d%%" "$percent"
 }
 
-printvolume_fontawesome()
+_printvolume_fontawesome()
 {
   # get evertyning within brackets
   local chstatus="$(amixer get Master | grep -oE '\[(.*?)\]' | tr -d []%)"
@@ -46,12 +46,22 @@ printvolume_fontawesome()
   env printf "$volumeicon%3d%%" "$percent"
 }
 
+_change_vol()
+{
+    # `5%+` or `5%-` to change up/down 5 percent
+    local percent="$1"
+    # alt
+    # amixer -q set Master $percent unmute 
+    wpctl set-volume @DEFAULT_AUDIO_SINK@ $percent
+
+    _updatei3bar
+}
 
 #local percents="2"
 case "$1" in
-  "up")   amixer -q set Master 2%+ unmute && updatei3bar;;
-  "down") amixer -q set Master 2%- unmute && updatei3bar;;
-  "togglemute") echo "TODO" ;;
-  "show") printvolume_fontawesome ;;
+  "--up")   _change_vol 2%+;;
+  "--down") _change_vol 2%-;;
+  "--togglemute") echo "TODO" ;;
+  "--show") _printvolume_fontawesome ;;
   *) echo "WAT?" ;;
 esac
